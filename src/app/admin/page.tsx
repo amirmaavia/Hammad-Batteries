4f2bba445befda1e6ef2e780fd8dae6d6973929a
Dell <maaviyamansoori222@gmail.com>
2026-05-14 02:05:20 +0500

    remove line 173 in page.tsx in admin

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -171,6 +171,7 @@ console.log(editingId, cleanItem);
 
   const handleReset = async () => {
     // await saveCatalogItems(DEFAULT_ITEMS);
+    //remove line
     const reloadedItems = await loadCatalogItems();
     setItems(reloadedItems);
     setEditingId(null);
